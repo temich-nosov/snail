ee5025bb7272ed09d4dbd14c4df7cb0539979e41
temich <lfenix@ya.ru>
2016-07-21 16:24:39 +0300

    Удалил ненужный код

--- headers/FullConnectedLayer.h
+++ headers/FullConnectedLayer.h
@@ -10,21 +10,4 @@ public:
   ~FullConnectedLayer();
 };
 
-/**
- * Слой активации
- */
-class ReluLayer : public Layer {
-  DataArray::Size inputOutputSize;
-public:
-  DataArray::Size getInputSize() const;
-  DataArray::Size getOutputSize() const;
-
-  ReluLayer(DataArray::Size size) : inputOutputSize(size) {}
-
-  virtual void propagate(const DataArray & input, DataArray & output);
-  virtual void backPropagate(const DataArray & input, const DataArray & output, const DataArray & error, DataArray & inputError, float lambda);
-
-  ~ReluLayer() {}
-};
-
 #endif
